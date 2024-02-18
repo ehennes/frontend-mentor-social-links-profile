8dd1c81a3466fda48dd5bee927f62db9e3b7c444
emhennes <emilyhennes1@gmail.com>
2024-02-17 16:54:14 -0800

    Initial commit.

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme'); 
+
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'gray': {
+            light: '#333333',
+            DEFAULT: '#1F1F1F',
+            dark: '#141414'
+        },
+        'green': '#c5f82a'
+    },
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
+  },
+  plugins: [],
+}
